ea89e3f9f08ee92ab91af56639846e67b12655dd
karan gerrard <karan.gerrard@gmail.com>
2025-06-11 00:35:07 +0530

    Updates

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 var vite_config_default = defineConfig({
+  base: "/raldang/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -147,7 +148,7 @@ var vite_config_default = defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true
   }
 });
