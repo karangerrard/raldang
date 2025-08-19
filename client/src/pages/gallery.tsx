9d5d8d1de1aced1b0f571fbb2277f428e74de9e3
karan gerrard <karan.gerrard@gmail.com>
2025-08-19 07:59:54 +0530

    Fixed gallery scroll to the top issue , now route from anywhere in the website , gallery page will open from the top

--- client/src/pages/gallery.tsx
+++ client/src/pages/gallery.tsx
@@ -1,7 +1,14 @@
 import Footer from "@/components/layout/footer";
 import Header from "@/components/layout/header";
 import GallerySection from "@/components/sections/gallery-section";
+import { useEffect } from "react";
 export default function GalleryPage() {
+
+  useEffect(() => {
+    // Scroll to the top of the page on mount
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
     <Header />
