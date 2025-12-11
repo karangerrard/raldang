d38eeba29f12aaf1278e0f8fce6192abbf5ce819
karan gerrard <karan.gerrard@gmail.com>
2025-12-11 17:29:11 +0530

    Added CNAME file and made changes for 404 page , now no 404 page should be visible when I deploy the code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,10 @@
+// Handle 404 redirect from GitHub Pages
+if (sessionStorage.redirect) {
+  const redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  window.location.href = redirect;
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
